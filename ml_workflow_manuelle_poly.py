7ec7d7a7c483740e380be31d36b9ba0389eea177
heus96 <felix-heuser@outlook.com>
2026-01-21 17:45:18 +0100

    resampeln glaube ist soweit durch

--- ml_workflow_manuelle_poly.py
+++ ml_workflow_manuelle_poly.py
@@ -23,7 +23,7 @@ print("Found band files:")
 for bf in band_files:
     print(f" - {bf.name}")
 
-# Referenzband für Resampling auswählen (erstes Band)
+# Referenzband für Resampling auswählen (erstes Band) hier kann man diskutieren ob man lieber die 10m Bänder oder 20m Bänder als Referenz nimmt !!!!!!!!!!!!!!!!!
 ref_band_path = [f for f in band_files if "B02" in f.name][0]  # Blaues Band (B02) als Referenz
 
 # Referenz laden 
@@ -32,6 +32,37 @@ with rasterio.open(ref_band_path) as ref:
     ref_transform = ref.transform
     ref_crs = ref.crs
 
+# print(ref_band_path.name)
+# print(ref_shape)
+
+# Alle Bänder laden und resamplen (src = Quelle, dst = Ziel)
+resampled_bands = []
+
+for band_file in band_files:
+    with rasterio.open(band_file) as src:
+        band = src.read(1).astype(np.float32)
+
+        # Fall 1: Band passt schon (umnötiges Resampling vermeiden)
+        if src.shape == ref_shape and src.transform == ref_transform:
+            resampled_bands.append(band)
+
+        # Fall 2: Band resamplen
+        else:
+            dst = np.empty(ref_shape, dtype=np.float32)
+
+            rasterio.warp.reproject(
+                source=band,
+                destination=dst,
+                src_transform=src.transform,
+                src_crs=src.crs,
+                dst_transform=ref_transform,
+                dst_crs=ref_crs,
+                resampling=rasterio.enums.Resampling.bilinear
+            )
+
+            resampled_bands.append(dst)
+
+print("Resampling done.")
 
 ####### Resample Block Ende ########
 
